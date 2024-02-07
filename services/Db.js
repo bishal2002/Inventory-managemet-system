5eba3f7147c775442a748aededefc5bb4ca35eac
BISAL RANJAN PADHAN <80083792+bishal2002@users.noreply.github.com>
2024-02-07 12:42:27 +0530

    Update Db.js

--- services/Db.js
+++ services/Db.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose')
-require('dotenv').config();
-const MONGO_URI = "mongodb+srv://<user>:<user>@cluster0.nrmaauw.mongodb.net/Inventory-Management?retryWrites=true&w=majority";
+// const mongoose = require('mongoose')
+// require('dotenv').config();
+// const MONGO_URI = "mongodb+srv://<user>:<user>@cluster0.nrmaauw.mongodb.net/Inventory-Management?retryWrites=true&w=majority";
 
-mongoose.set('strictQuery', false);
-const connectDB = async () => {
-    try {
-        mongoose.connect(MONGO_URI, { 
-            useNewUrlParser: true,});
-            console.log('MongoDB Connected...') 
-        } catch (err) {
-            console.error(err.message); 
-            process.exit(1);
-        }
-    };
+// mongoose.set('strictQuery', false);
+// const connectDB = async () => {
+//     try {
+//         mongoose.connect(MONGO_URI, { 
+//             useNewUrlParser: true,});
+//             console.log('MongoDB Connected...') 
+//         } catch (err) {
+//             console.error(err.message); 
+//             process.exit(1);
+//         }
+//     };
 
-module.exports = connectDB;
+// module.exports = connectDB;
